49bdba84771517a24a20770f7fe9d8370ce9517b
Arthur Terozendi <arthurterozendi@gmail.com>
2021-04-14 18:42:52 -0300

    Classes de leitura e escrita

---
+++ src/ERE/SistemasOperacionais/Leitor.java
@@ -0,0 +1,26 @@
+package ERE.SistemasOperacionais;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Leitor {
+    private File file;
+
+    public Leitor(String path) {
+        this.file = new File(path);
+    }
+
+    public ArrayList<String> ler() throws IOException {
+        ArrayList<String> entrada = new ArrayList<>();
+
+        FileReader leitor = new FileReader(file);
+        BufferedReader bLeitor =  new BufferedReader(leitor);
+
+        while(bLeitor.ready()) {
+            entrada.add(bLeitor.readLine());
+        }
+
+        return entrada;
+    }
+
+}
